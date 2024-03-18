1bcf2d1657221614d4cae004bce1884221885952
NATALIA REBECA LARA ROBLES <nrlara@alu.ucam.edu>
2024-03-18 10:29:10 +0100

    Creada clase estatica

--- SistemaGestionEmpleados.java
+++ SistemaGestionEmpleados.java
@@ -4,27 +4,26 @@ public class SistemaGestionEmpleados {
 
 
     public static void main(String[] args) {
-        
-            Empleados empleados = new Empleados(3);
-    
-            Empleado empleado1 = new Empleado("Juan", "Desarrollador", 50000);
-            Empleado empleado2 = new Empleado("María", "Diseñadora", 45000);
-            Empleado empleado3 = new Empleado("Pedro", "Gerente", 60000);
-    
-            empleados.darDeAltaEmpleado(empleado1, 0);
-            empleados.darDeAltaEmpleado(empleado2, 1);
-            empleados.darDeAltaEmpleado(empleado3, 2);
-    
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("Introduzca el porcentaje de aumento de salario: ");
-            double porcentaje = scanner.nextDouble();
-    
-            empleados.aumentarSalario(porcentaje);
-    
-            System.out.println("Lista de Empleados:");
-            empleados.mostrarEmpleados();
-    
-            scanner.close();
-        }
+        Empleados empleados = new Empleados(3);
+
+        Empleado empleado1 = new Empleado(Textos.NOMBRE_EMPLEADO1, Textos.CARGO_EMPLEADO1, Textos.SALARIO_EMPLEADO1);
+        Empleado empleado2 = new Empleado(Textos.NOMBRE_EMPLEADO2, Textos.CARGO_EMPLEADO2, Textos.SALARIO_EMPLEADO2);
+        Empleado empleado3 = new Empleado(Textos.NOMBRE_EMPLEADO3, Textos.CARGO_EMPLEADO3, Textos.SALARIO_EMPLEADO3);
+
+        empleados.darDeAltaEmpleado(empleado1, 0);
+        empleados.darDeAltaEmpleado(empleado2, 1);
+        empleados.darDeAltaEmpleado(empleado3, 2);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print(Textos.PEDIR_PORCENTAJE_AUMENTO);
+        double porcentaje = scanner.nextDouble();
+
+        empleados.aumentarSalario(porcentaje);
+
+        System.out.println(Textos.LISTA_EMPLEADOS);
+        empleados.mostrarEmpleados();
+
+        scanner.close();
+    }
     }
 
---
+++ Textos.java
@@ -0,0 +1,17 @@
+public class Textos {
+    public static final String NOMBRE_EMPLEADO1 = "Juan";
+    public static final String NOMBRE_EMPLEADO2 = "María";
+    public static final String NOMBRE_EMPLEADO3 = "Pedro";
+
+    public static final String CARGO_EMPLEADO1 = "Desarrollador";
+    public static final String CARGO_EMPLEADO2 = "Diseñadora";
+    public static final String CARGO_EMPLEADO3 = "Gerente";
+
+    public static final double SALARIO_EMPLEADO1 = 50000;
+    public static final double SALARIO_EMPLEADO2 = 45000;
+    public static final double SALARIO_EMPLEADO3 = 60000;
+
+    public static final String PEDIR_PORCENTAJE_AUMENTO = "Introduzca el porcentaje de aumento de salario: ";
+    public static final String LISTA_EMPLEADOS = "Lista de Empleados:";
+    
+}
